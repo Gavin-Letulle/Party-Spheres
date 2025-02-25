0e10fd7da1c1bf936615e05517cef4deb4899929
Gekire156 <gavin.letulle@icloud.com>
2025-02-24 18:00:54 -0600

    database comments

--- database/connection.js
+++ database/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'root',
-    password: 'gavsql156', //Change password to your mysql password (might be your actual computer login password, not sure how to make this global yet.)
-    database: 'cooking',
+    user: 'root', //We will probably want to change root to something else, not sure how that works
+    password: 'gavsql156', //Change password to your mysql password (might be your actual computer login password, not sure how to make this global yet)
+    database: 'partyspheres', //Make sure this is the same name as the database
 });
 
 connection.connect((err => {
