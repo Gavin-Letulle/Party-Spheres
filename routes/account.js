7adb8b6c3f55a430cb0be5ff5b00dd5651a7f88c
Gekire156 <gavin.letulle@icloud.com>
2025-03-03 22:06:27 -0600

    view and edit account

--- routes/account.js
+++ routes/account.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
   res.render('account', { title: 'Account' });
 });
 
+router.get('/edit', function(req, res) {
+  res.render('edit', { title: 'Edit' });
+});
+
 module.exports = router;
