924b1d65a60f8f2e53370fcb4b3b34462bb5ca01
Gekire156 <gavin.letulle@icloud.com>
2025-03-08 11:10:25 -0600

    Happiness bar

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -15,3 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+function updateHealthBar(percentage) {
+    const healthBar = document.getElementById("healthBar");
+    healthBar.style.setProperty("--health", percentage + "%");
+    if (percentage >= 75) {
+        healthBar.style.backgroundColor = "#73ff8f";
+    } else if (percentage >= 50) {
+        healthBar.style.backgroundColor = "#f8ff73";
+    } else if (percentage >= 25) {
+        healthBar.style.backgroundColor = "#ffc74f";
+    } else {
+        healthBar.style.backgroundColor = "#f57979";
+    }
+}
+updateHealthBar(50);
