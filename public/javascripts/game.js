23104b3e01b5a7651f5d8f2c2fa2e09ce58bdb16
Gekire156 <gavin.letulle@icloud.com>
2025-03-08 13:42:52 -0600

    rainbow health bar

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -19,14 +19,21 @@ document.addEventListener("DOMContentLoaded", function () {
 function updateHealthBar(percentage) {
     const healthBar = document.getElementById("healthBar");
     healthBar.style.setProperty("--health", percentage + "%");
-    if (percentage >= 75) {
-        healthBar.style.backgroundColor = "#73ff8f";
-    } else if (percentage >= 50) {
-        healthBar.style.backgroundColor = "#f8ff73";
-    } else if (percentage >= 25) {
-        healthBar.style.backgroundColor = "#ffc74f";
+
+    if (percentage === 100) {
+        healthBar.classList.add("rainbow-effect");
     } else {
-        healthBar.style.backgroundColor = "#f57979";
+        healthBar.classList.remove("rainbow-effect");
+        
+        if (percentage >= 75) {
+            healthBar.style.backgroundColor = "#73ff8f";
+        } else if (percentage >= 50) {
+            healthBar.style.backgroundColor = "#f8ff73";
+        } else if (percentage >= 25) {
+            healthBar.style.backgroundColor = "#ffc74f";
+        } else {
+            healthBar.style.backgroundColor = "#f57979";
+        }
     }
 }
 updateHealthBar(50);
